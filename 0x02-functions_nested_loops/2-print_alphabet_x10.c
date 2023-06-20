7ce38461a752fe26e8d69ec0c315cf7aa52fea6f
strawhatayoub <ayoub.mouaki.2000@gmail.com>
2023-06-20 07:32:59 -0700

    All tasks are complleted

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void print_alphabet_x10(void) {
+    int count = 0;
+
+    while (count < 10) {
+        char letter = 'a';
+
+        while (letter <= 'z') {
+            _putchar(letter);
+            letter++;
+        }
+
+        count++;
+    }
+
+    _putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,7 @@
+int _islower(int c) {
+    if (c >= 'a' && c <= 'z') {
+        return 1; // Character is lowercase
+    } else {
+        return 0; // Character is not lowercase
+    }
+}
