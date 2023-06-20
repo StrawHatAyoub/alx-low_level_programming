4428d240b64b6d8ff2d0f11cab5d4cd15cecf376
strawhatayoub <ayoub.mouaki.2000@gmail.com>
2023-06-20 07:55:19 -0700

    All tasks are completed

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,14 +1,8 @@
-#include <stdio.h>
+#include "main.h"
 
 int print_sign(int n) {
     if (n > 0) {
         printf("+");
-        return 1; // Positive number
-    } else if (n < 0) {
-        printf("-");
-        return -1; // Negative number
-    } else {
-        printf("0");
-        return 0; // Zero
-    }
+        return 1;
+    } else if (n == 0)
 }
